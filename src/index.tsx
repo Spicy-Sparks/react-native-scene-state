00f4e55d09f0295cdd888a97a7584dd0eb1260cd
Marco Fontana <fontana.marco@hotmail.com>
2021-05-31 17:14:04 +0200

    Update index.tsx

--- src/index.tsx
+++ src/index.tsx
@@ -1,9 +1,35 @@
-import { NativeModules } from 'react-native';
+import {
+  EmitterSubscription,
+  NativeEventEmitter,
+  NativeModules,
+} from 'react-native';
+
+type SceneStateStateType = "active" | "inactive" | "background"
 
 type SceneStateType = {
-  multiply(a: number, b: number): Promise<number>;
+  getCurrentState(): Promise<SceneStateStateType>;
+  addEventListener(event: string, listener: (event: any) => any): EmitterSubscription;
+  removeEventListener(listener: EmitterSubscription): void;
+  removeAllListeners(event: string): void;
 };
 
 const { SceneState } = NativeModules;
+
+const emitter = new NativeEventEmitter(SceneState);
+
+SceneState.addEventListener = function (
+  event: string,
+  listener: (event: any) => any
+): EmitterSubscription {
+  return emitter.addListener(event, listener);
+};
+
+SceneState.removeEventListener = function (listener: EmitterSubscription) {
+  return emitter.removeSubscription(listener);
+};
+
+SceneState.removeAllListeners = function (event: string) {
+  return emitter.removeAllListeners(event);
+};
 
 export default SceneState as SceneStateType;
