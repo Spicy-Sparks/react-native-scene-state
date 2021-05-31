3b13922c11154566de9300663ab5f6c12504d80d
Marco Fontana <fontana.marco@hotmail.com>
2021-05-31 17:13:36 +0200

    Update SceneState.h

--- ios/SceneState.h
+++ ios/SceneState.h
@@ -1,5 +1,9 @@
+#import <React/RCTEventEmitter.h>
 #import <React/RCTBridgeModule.h>
 
-@interface SceneState : NSObject <RCTBridgeModule>
+@import UIKit;
+
+API_AVAILABLE(ios(13.0))
+@interface SceneState : RCTEventEmitter<RCTBridgeModule>
 
 @end
